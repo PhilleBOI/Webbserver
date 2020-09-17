74853fee97e882e400eefe329480e6fe7a7b47ed
PhilleBOI <philip.thilen@elev.ga.ntig.se>
2020-09-17 13:16:18 +0200

    mongoDB

---
+++ MyFirstServer/mongoDBTest.js
@@ -0,0 +1,40 @@
+// getting-started.js
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
+const kittySchema = new mongoose.Schema({
+    name: String
+})
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+kittySchema.methods.speak = function () {
+    const greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name";
+    console.log(greeting);
+}
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+});
+
+Kitten.find(function (err, kitten) {
+    if (err) return console.error(err);
+    console.log(kittens);
+})
+
+Kitten.find({ name: /^fluff/ }, callback);
